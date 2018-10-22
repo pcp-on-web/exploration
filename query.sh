79619bbdc06410b1482235d7ac638f157c7dd1a2
triechert <thomas.riechert@htwk-leipzig.de>
2018-10-22 18:43:15 +0200

    Initial script

---
+++ query.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cat $1 > /dev/tty
+
+sparql=$(head -$(grep -n "===DESCRIPTION===" $1 | cut -d":" -f1 )  $1 | grep -v === | tr -c "[:print:]" " " ) 
+
+echo java -jar kbox-v0.0.1-alpha3-RC16.jar -kb "http://purl.org/pcp-on-web/ontology,http://purl.org/pcp-on-web/dataset" -sparql \'$sparql\' > cmd.sh.tmp
+bash cmd.sh.tmp
