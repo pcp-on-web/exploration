cdd6aa17b766a20cfc8c313758ccd7d485b52642
Edgard Marx <digamarx@gmail.com>
2018-10-25 13:55:19 +0200

    fixing problem of returning null results
    adding the tag -install

--- query.sh
+++ query.sh
@@ -4,5 +4,5 @@ cat $1 > /dev/tty
 
 sparql=$(head -$(grep -n "===DESCRIPTION===" $1 | cut -d":" -f1 )  $1 | grep -v === | tr -c "[:print:]" " " ) 
 
-echo java -jar kbox-v0.0.1-alpha3-RC16.jar -kb "http://purl.org/pcp-on-web/ontology,http://purl.org/pcp-on-web/dataset" -sparql \'$sparql\' > cmd.sh.tmp
+echo java -jar kbox-v0.0.1-alpha3-RC16.jar -kb "http://purl.org/pcp-on-web/ontology,http://purl.org/pcp-on-web/dataset" -sparql \'$sparql\' -install > cmd.sh.tmp
 bash cmd.sh.tmp
